00ebd86b47fd4d0bb1dab699717bb7c460578d49
Simon Luijk <simon@simonluijk.com>
2017-03-05 10:32:17 +0100

    Also output role names

--- output.tf
+++ output.tf
@@ -2,10 +2,18 @@ output "instance_role" {
     value = "${aws_iam_role.ecs_instance.arn}"
 }
 
+output "instance_role_name" {
+    value = "${aws_iam_role.ecs_instance.name}"
+}
+
 output "instance_profile" {
   value = "${aws_iam_instance_profile.ecs_instance.arn}"
 }
 
 output "service_role" {
     value = "${aws_iam_role.ecs_service.arn}"
+}
+
+output "service_role_name" {
+    value = "${aws_iam_role.ecs_service.name}"
 }
